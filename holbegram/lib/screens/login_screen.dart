3bcc1d1d2918e62b3ce206eece3b709aed8193e6
MarcP70 <marcpourias@yahoo.fr>
2024-11-18 16:16:32 +0100

    Update login_screen.dart to add signup controllers

--- holbegram/lib/screens/login_screen.dart
+++ holbegram/lib/screens/login_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:holbegram/widgets/text_field.dart';
+import 'signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
-  // Attributes for email and password controllers
   final TextEditingController emailController;
+  final TextEditingController usernameController;
   final TextEditingController passwordController;
+  final TextEditingController passwordConfirmController;
 
   const LoginScreen({
     super.key,
     required this.emailController,
+    required this.usernameController,
     required this.passwordController,
+    required this.passwordConfirmController,
   });
 
   @override
@@ -17,22 +21,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  // Visibility toggle for password field
-  bool _passwordVisible = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _passwordVisible = false; // Set initial state for password visibility
-  }
-
-  @override
-  void dispose() {
-    // Dispose of the TextEditingControllers
-    widget.emailController.dispose();
-    widget.passwordController.dispose();
-    super.dispose();
-  }
+  bool _passwordVisible = false;
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +33,6 @@ class _LoginScreenState extends State<LoginScreen> {
           mainAxisSize: MainAxisSize.min,
           children: [
             const SizedBox(height: 28),
-            // App Title
             const Text(
               'Holbegram',
               style: TextStyle(
@@ -53,19 +41,16 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             const SizedBox(height: 20),
-            // Logo
             const Image(
               width: 80,
               height: 60,
               image: AssetImage('assets/images/logo.webp'),
             ),
             const SizedBox(height: 20),
-            // Padding for input fields
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: Column(
                 children: [
-                  // Email TextField
                   const SizedBox(height: 28),
                   TextFieldInput(
                     controller: widget.emailController,
@@ -74,14 +59,12 @@ class _LoginScreenState extends State<LoginScreen> {
                     keyboardType: TextInputType.emailAddress,
                   ),
                   const SizedBox(height: 24),
-                  // Password TextField
                   TextFieldInput(
                     controller: widget.passwordController,
                     isPassword: !_passwordVisible,
                     hintText: 'Password',
                     keyboardType: TextInputType.visiblePassword,
                     suffixIcon: IconButton(
-                      alignment: Alignment.center,
                       icon: Icon(
                         _passwordVisible
                             ? Icons.visibility
@@ -95,7 +78,6 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                   const SizedBox(height: 28),
-                  // Login Button
                   SizedBox(
                     height: 48,
                     width: double.infinity,
@@ -115,29 +97,29 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                   ),
                   const SizedBox(height: 24),
-                  // Forgot details row
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text('Forgot your login details?'),
-                      Text(
-                        ' Get help logging in',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 24),
-                  // Divider
                   const Divider(thickness: 2),
-                  const Padding(
-                    padding: EdgeInsets.symmetric(vertical: 12),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12),
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Text("Don't have an account? "),
+                        const Text("Don't have an account? "),
                         TextButton(
-                          onPressed: null, // Sign up functionality to be added
-                          child: Text(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => SignupScreen(
+                                  emailController: widget.emailController,
+                                  usernameController: widget.usernameController,
+                                  passwordController: widget.passwordController,
+                                  passwordConfirmController:
+                                      widget.passwordConfirmController,
+                                ),
+                              ),
+                            );
+                          },
+                          child: const Text(
                             'Sign up',
                             style: TextStyle(
                               fontWeight: FontWeight.bold,
@@ -147,35 +129,6 @@ class _LoginScreenState extends State<LoginScreen> {
                         ),
                       ],
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  // Row with "OR" and Dividers
-                  const Row(
-                    children: [
-                      Flexible(child: Divider(thickness: 2)),
-                      Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8),
-                        child: Text('OR'),
-                      ),
-                      Flexible(child: Divider(thickness: 2)),
-                    ],
-                  ),
-                  const SizedBox(height: 10),
-                  // Google Sign-In Row
-                  const Row(
-                    mainAxisSize: MainAxisSize.min,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image(
-                        width: 40,
-                        height: 40,
-                        image: NetworkImage(
-                          'https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png',
-                        ),
-                      ),
-                      SizedBox(width: 10),
-                      Text('Sign in with Google'),
-                    ],
                   ),
                 ],
               ),
