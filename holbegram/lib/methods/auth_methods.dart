87ac68f18e765a3201fd08ead9a8d1e88d4105fc
MarcP70 <marcpourias@yahoo.fr>
2024-11-19 09:59:13 +0100

    Add auth_methods.dart to add the authentification methods for login and signup

---
+++ holbegram/lib/methods/auth_methods.dart
@@ -0,0 +1,82 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:typed_data'; // For handling the file (profile picture)
+import '../models/user.dart'; // Import the Users class
+
+class AuthMethods {
+  // Firebase authentication and Firestore instances
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Login Method
+  Future<String> login({
+    required String email,
+    required String password,
+  }) async {
+    String res = "An error occurred";
+    try {
+      if (email.isEmpty || password.isEmpty) {
+        res = "Please fill all the fields";
+      } else {
+        // Use FirebaseAuth to sign in
+        await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+        res = "success";
+      }
+    } catch (err) {
+      res = err.toString(); // Return error message in case of failure
+    }
+    return res;
+  }
+
+  // Sign Up Method
+  Future<String> signUpUser({
+    required String email,
+    required String password,
+    required String username,
+    Uint8List? file, // Nullable for handling profile picture
+  }) async {
+    String res = "An error occurred";
+    try {
+      if (email.isEmpty || password.isEmpty || username.isEmpty) {
+        res = "Please fill all the fields";
+      } else {
+        // Create a new user with Firebase Authentication
+        UserCredential userCredential =
+            await _auth.createUserWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+
+        // Getting the created user
+        User? user = userCredential.user;
+
+        if (user != null) {
+          // Create a new Users instance
+          Users users = Users(
+            uid: user.uid,
+            email: email,
+            username: username,
+            bio: '',
+            photoUrl: '', // You can add a profile photo upload process later
+            followers: [],
+            following: [],
+            posts: [],
+            saved: [],
+            searchKey: username.substring(0, 1).toUpperCase(),
+          );
+
+          // Save the user information to Firestore
+          await _firestore
+              .collection("users")
+              .doc(user.uid)
+              .set(users.toJson());
+          res = "success";
+        }
+      }
+    } catch (err) {
+      res = err.toString(); // Return error message in case of failure
+    }
+    return res;
+  }
+}
