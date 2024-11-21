495b4b09c9702e75d8046d50b8e1887599319d6f
MarcP70 <marcpourias@yahoo.fr>
2024-11-21 11:44:08 +0100

    Update main.dart to change the import of login_screen.dart

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:holbegram/screens/login_screen.dart';
+import './screens/login_screen.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
