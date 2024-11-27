b66432b7e76be2c4c4eda3ad7e7e592fb7aa7b1e
MarcP70 <marcpourias@yahoo.fr>
2024-11-27 15:04:09 +0100

    Add search.dart for the search page

---
+++ holbegram/lib/screens/pages/search.dart
@@ -0,0 +1,16 @@
+// search.dart
+import 'package:flutter/material.dart';
+
+class Search extends StatelessWidget {
+  const Search({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Search Page',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
