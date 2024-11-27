a26082a206caee9d4d16e458058dc3bef67d50a2
MarcP70 <marcpourias@yahoo.fr>
2024-11-27 15:03:46 +0100

    Add profile_screene.dart for the profile page

---
+++ holbegram/lib/screens/pages/profile_screen.dart
@@ -0,0 +1,16 @@
+// profile_screen.dart
+import 'package:flutter/material.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Profile Page',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
