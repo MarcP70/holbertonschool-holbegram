558d3238d1fe6583cb7ad4ee94970edb72df8c18
MarcP70 <marcpourias@yahoo.fr>
2024-11-27 15:03:02 +0100

    Add favorite.dart for the favorite page

---
+++ holbegram/lib/screens/pages/favorite.dart
@@ -0,0 +1,16 @@
+// favorite.dart
+import 'package:flutter/material.dart';
+
+class Favorite extends StatelessWidget {
+  const Favorite({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Favorite Page',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
