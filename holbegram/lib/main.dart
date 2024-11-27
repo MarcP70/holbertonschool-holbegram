bb5fe063b3f58d50ce6e867d6ca6db65a2a723ae
MarcP70 <marcpourias@yahoo.fr>
2024-11-27 14:55:49 +0100

    Update main.dart to implement the provider

--- holbegram/lib/main.dart
+++ holbegram/lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart'; // Ajout du package Provider
 import './screens/login_screen.dart';
+import './screens/home.dart';
+import './providers/user_provider.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,13 +38,25 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Holbegram',
-      home: LoginScreen(
-        emailController: emailController,
-        passwordController: passwordController,
-        usernameController: usernameController,
-        passwordConfirmController: passwordConfirmController,
+    return MultiProvider(
+      providers: [
+        // Configuration du provider pour la gestion de l'utilisateur
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Holbegram',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        // Définir la page de connexion par défaut
+        home: LoginScreen(
+          emailController: emailController,
+          passwordController: passwordController,
+          usernameController: usernameController,
+          passwordConfirmController: passwordConfirmController,
+        ),
       ),
     );
   }
