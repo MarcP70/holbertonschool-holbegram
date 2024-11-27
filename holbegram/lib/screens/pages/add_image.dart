ab61d14cfdecb1f410469e7a8ca361e3ebda14fd
MarcP70 <marcpourias@yahoo.fr>
2024-11-27 15:02:31 +0100

    Add add_image.dart to add an image

---
+++ holbegram/lib/screens/pages/add_image.dart
@@ -0,0 +1,16 @@
+// add_image.dart
+import 'package:flutter/material.dart';
+
+class AddImage extends StatelessWidget {
+  const AddImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Add Image Page',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
