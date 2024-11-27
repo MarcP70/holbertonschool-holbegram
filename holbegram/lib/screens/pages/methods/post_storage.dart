85ceab472a50772136dba4e640d5066d3e8fa93c
MarcP70 <marcpourias@yahoo.fr>
2024-11-27 18:55:31 +0100

    Add post_storage.dart to store a post

---
+++ holbegram/lib/screens/pages/methods/post_storage.dart
@@ -0,0 +1,60 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:uuid/uuid.dart';
+import 'package:flutter/foundation.dart';
+import '../../../screens/auth/methods/user_storage.dart';
+
+class PostStorage {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Method to upload a post
+  Future<String> uploadPost(
+    String caption,
+    String uid,
+    String username,
+    String profImage,
+    Uint8List image,
+  ) async {
+    try {
+      // Generate a unique postId
+      String postId = const Uuid().v4();
+
+      // Upload image to storage and get URL
+      String postUrl = await StorageMethods().uploadImageToStorage(
+        'posts', // Specify the folder for storing posts
+        image, // Pass the image as Uint8List
+        true, // Specify that it's a post
+      );
+
+      // Create post data
+      Map<String, dynamic> postData = {
+        'caption': caption,
+        'uid': uid,
+        'username': username,
+        'profImage': profImage,
+        'postId': postId,
+        'postUrl': postUrl,
+        'datePublished': DateTime.now(),
+        'likes': [], // Default empty likes list
+      };
+
+      // Add post to Firestore
+      await _firestore.collection('posts').doc(postId).set(postData);
+
+      return 'Ok';
+    } catch (e) {
+      // Return error message
+      return e.toString();
+    }
+  }
+
+  // Method to delete a post
+  Future<void> deletePost(String postId) async {
+    try {
+      // Delete post from Firestore
+      await _firestore.collection('posts').doc(postId).delete();
+    } catch (e) {
+      debugPrint('Error deleting post: $e');
+      rethrow; // Re-throw the error if needed
+    }
+  }
+}
